d8ab57e3b4b37efbe029c3f4088a4200a65afc35
juhyeong <169asd@naver.com>
2023-05-06 23:13:12 +0900

    new file

---
+++ 수정코드2.cpp
@@ -0,0 +1,222 @@
+
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+#include<time.h>
+#include<stdbool.h>
+
+#define DINO_BOTTOM_Y 12
+#define TREE_BOTTOM_Y 20
+#define TREE_BOTTOM_X 45
+#define BIRD_BOTTOM_Y 10
+
+
+void SetConsoleView()
+{
+	system("mode con:cols=100 lines=25");
+	system("title Google Dinosaurs. By BlockDMask.");
+}
+
+
+void GotoXY(int x, int y)
+{
+	COORD Pos;
+	Pos.X = 2 * x;
+	Pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
+
+int GetKeyDown()
+{
+	if (_kbhit() != 0)
+	{
+		return _getch();
+	}
+	return 0;
+}
+
+
+void DrawDino(int dinoY)
+{
+	GotoXY(0, dinoY);
+	static bool legFlag = true;
+	printf("        $$$$$$$ \n");
+	printf("       $$ $$$$$$\n");
+	printf("       $$$$$$$$$\n");
+	printf("$      $$$      \n");
+	printf("$$     $$$$$$$  \n");
+	printf("$$$   $$$$$     \n");
+	printf(" $$  $$$$$$$$$$ \n");
+	printf(" $$$$$$$$$$$    \n");
+	printf("  $$$$$$$$$$    \n");
+	printf("    $$$$$$$$    \n");
+	printf("     $$$$$$     \n");
+	if (legFlag)
+	{
+		printf("     $    $$$    \n");
+		printf("     $$          ");
+		legFlag = false;
+	}
+	else
+	{
+		printf("     $$$  $     \n");
+		printf("          $$    ");
+		legFlag = true;
+	}
+}
+
+void DrawTree(int treeX)
+{
+	GotoXY(treeX, TREE_BOTTOM_Y);
+	printf("$$$$");
+	GotoXY(treeX, TREE_BOTTOM_Y + 1);
+	printf(" $$ ");
+	GotoXY(treeX, TREE_BOTTOM_Y + 2);
+	printf(" $$ ");
+	GotoXY(treeX, TREE_BOTTOM_Y + 3);
+	printf(" $$ ");
+	GotoXY(treeX, TREE_BOTTOM_Y + 4);
+	printf(" $$ ");
+}
+
+void DrawBird(int birdX)
+{
+	GotoXY(birdX, BIRD_BOTTOM_Y);
+	printf("  $$$$$   ");
+	GotoXY(birdX, BIRD_BOTTOM_Y + 1);
+	printf(" ($$   $$) ");
+}
+
+void DrawObstacle(int obstacleType, int obstacleX)
+{
+	if (obstacleType == 1)
+	{
+		DrawTree(obstacleX);
+	}
+	else if (obstacleType == 2)
+	{
+		DrawBird(obstacleX);
+	}
+}
+
+void DrawGameOver(const int score)
+{
+	system("cls");
+	int x = 18;
+	int y = 8;
+	GotoXY(x, y);
+	printf("===========================");
+	GotoXY(x, y + 1);
+	printf("======G A M E O V E R======");
+	GotoXY(x, y + 2);
+	printf("===========================");
+	GotoXY(x, y + 5);
+	printf("SCORE : %d", score);
+
+	printf("\n\n\n\n\n\n\n\n\n");
+	system("pause");
+}
+
+
+bool isCollision(const int obstacleType, const int obstacleX, const int dinoY)
+{
+	if (obstacleType == 1)
+	{
+		if (obstacleX <= 8 && obstacleX >= 4 &&
+			dinoY > 8)
+		{
+			return true;
+		}
+	}
+	else if (obstacleType == 2)
+	{
+		if (obstacleX <= 8 && obstacleX >= 2 &&
+			dinoY >= BIRD_BOTTOM_Y && dinoY < BIRD_BOTTOM_Y + 2)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	SetConsoleView();
+
+	while (true)		//(v2.0) 게임 루프
+	{
+		//게임 시작시 초기화
+		bool isJumping = false;
+		bool isBottom = true;
+		const int gravity = 3;
+
+		int dinoY = DINO_BOTTOM_Y;
+		int obstacleX = TREE_BOTTOM_X;
+		int obstacleType = 0;
+
+		int score = 0;
+		clock_t start, curr;	//점수 변수 초기화
+		start = clock();		//시작시간 초기화
+
+		while (true)	//한 판에 대한 루프
+		{
+			if (isCollision(obstacleType, obstacleX, dinoY))
+				break;
+
+			//z키가 눌렸고, 바닥이 아닐때 점프
+			if (GetKeyDown() == 'z' && isBottom)
+			{
+				isJumping = true;
+				isBottom = false;
+			}
+
+			if (isJumping)
+			{
+				dinoY -= gravity;
+			}
+			else
+			{
+				dinoY += gravity;
+			}
+
+			if (dinoY >= DINO_BOTTOM_Y)
+			{
+				dinoY = DINO_BOTTOM_Y;
+				isBottom = true;
+			}
+
+			obstacleX -= 2;
+			if (obstacleX <= 0)
+			{
+				obstacleType = rand() % 2 + 1;
+				obstacleX = TREE_BOTTOM_X;
+			}
+
+			if (dinoY <= 3)
+			{
+				isJumping = false;
+			}
+
+			DrawDino(dinoY);		//draw dino
+			DrawObstacle(obstacleType, obstacleX);		//draw obstacle
+
+			//(v2.0)
+			curr = clock();
+			if (((curr - start) / CLOCKS_PER_SEC) >= 1)
+			{
+				score++;	//스코어 UP
+				start = clock();	//시작시간 초기화
+			}
+			Sleep(60);
+			system("cls");	//clear
+
+			GotoXY(22, 0);
+			printf("Score : %d ", score);
+		}
+
+		//(v2.0) 게임 오버 메뉴
+		DrawGameOver(score);
+	}
+	return 0;
+}
